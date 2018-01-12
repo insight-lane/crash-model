5abb5415a8e0d9b1167204d34cbeb0cf5e624853
Jenny Turner-Trauring <jenineturner@gmail.com>
2018-01-11 19:05:01 -0500

    Add more steps to make_dataset_osm

--- src/data/make_dataset_osm.py
+++ src/data/make_dataset_osm.py
@@ -12,11 +12,31 @@ if __name__ == '__main__':
 
     city = 'Boston, Massachusetts, USA'
 
+    # Get the maps out of open street map, both projections
     subprocess.check_call([
         'python',
         '-m',
         'data.osm_create_maps',
         city,
         DATA_FP
+    ])
+
+    subprocess.check_call([
+        'python',
+        '-m',
+        'data.extract_intersections',
+        DATA_FP + '/processed/maps/osm_ways.shp',
+        '-d',
+        DATA_FP
+    ])
+
+    subprocess.check_call([
+        'python',
+        '-m',
+        'data.create_segments',
+        '-d',
+        DATA_FP,
+        '-r',
+        DATA_FP + '/processed/maps/osm_ways_3857.shp'
     ])
 
