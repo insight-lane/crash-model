4b76c80d08b97907f5968341461b6867a60054bb
Ben Batorsky <yksrotab@gmail.com>
2018-02-04 11:20:15 -0500

    working version of modularized model

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -26,30 +26,46 @@ BASE_DIR = os.path.dirname(
 DATA_FP = BASE_DIR + '/data/processed/'
 
 # parse arguments
-parser = argparse.ArgumentParser(description="Train crash model")
+parser = argparse.ArgumentParser(description="Train crash model.  Additional features specify '' to not include")
 parser.add_argument("-m", "--modelname", nargs="+", 
 					default='LR_base',
                     help="name of the model, for consistency")
 parser.add_argument("-seg", "--seg_data", nargs="+", 
 					default=DATA_FP+'vz_predict_dataset.csv.gz',
-                    help="path to the segment data (see data standards)")
+                    help="path to the segment data (see data standards) default vz_predict_dataset.csv.gz")
+parser.add_argument("-concern", "--concern_column", nargs="+", 
+					default='concern',
+                    help="Feature name of concern data in segment data.  Let equal '' to not include. default 'concern'")
 parser.add_argument("-atr", "--atr_data", nargs="+", 
 					default=DATA_FP+'atrs_predicted.csv',
-                    help="path to the ATR data (see data standards)")
+                    help="path to the ATR data (see data standards) default atrs_predicted.csv")
+parser.add_argument("--atr_columns", nargs="+", 
+					default=['speed_coalesced', 'volume_coalesced'],
+                    help="features from atr file")
 parser.add_argument("-tmc", "--tmc_data", nargs="+", 
 					default=DATA_FP+'tmc_summary.json',
-                    help="path to the TMC data (see data standards)")
+                    help="path to the TMC data (see data standards) default tmc_summary.json")
+parser.add_argument("--tmc_columns", nargs="+", 
+					default=['Conflict'],
+                    help="features from tmc file")
 parser.add_argument("-time", "--time_target", nargs="+",
 					default=tuple([19,2017]),
                     help="tuple (week,year) for prediction target")
-parser.add_argument("-features", "--features", nargs="+",
-					default=['AADT', 'SPEEDLIMIT', 'Struct_Cnd', 'Surface_Tp', 'F_F_Class'],
+parser.add_argument("-f_cat", "--features_categorical", nargs="+",
+					default=['SPEEDLIMIT', 'Struct_Cnd', 'Surface_Tp', 'F_F_Class'],
+                    help="list of categorical segment features")
+parser.add_argument("-f_cont", "--features_continuous", nargs="+",
+					default=['AADT'],
                     help="list of segment features to incude")
+parser.add_argument("-process", "--process_features", nargs="+",
+					default=True,
+                    help="Make categorical into dummies, standardize continuous")
 args = parser.parse_args()
 
-week, year = args.time_target
-SEG_CHARS = args.features
-
+week = int(args.time_target[0])
+year = int(args.time_target[1])
+f_cat = args.features_categorical
+f_cont = args.features_continuous
 
 # Read in data
 data = pd.read_csv(args.seg_data, dtype={'segment_id':'str'})
@@ -58,56 +74,67 @@ data.sort_values(['segment_id', 'year', 'week'], inplace=True)
 data_nonzero = data.set_index('segment_id').loc[data.groupby('segment_id').crash.sum()>0]
 data_nonzero.reset_index(inplace=True)
 # segment chars
-data_segs = data_nonzero.groupby('segment_id')[SEG_CHARS].max()  # grab the highest values from each column for a segment, not used in model?
+data_segs = data_nonzero.groupby('segment_id')[f_cont+f_cat].max()  # grab the highest values from each column
 data_segs.reset_index(inplace=True)
 
-# simple add concern, any concern reported 2016
-concern_observed = data_nonzero[data_nonzero.year==2016].groupby('segment_id').concern.max()
-concern_observed.name = 'concern_observed'
+# create featureset holder
+features = f_cont+f_cat
+print('Segment features included: {}'.format(features))
+
+# create lagged crash values
+crash_lags = format_crash_data(data_nonzero.set_index(['segment_id','year','week']), 'crash', 
+	week, year)
+data_model = crash_lags.merge(data_segs, left_on='segment_id', right_on='segment_id')
+
+# add concern
+if args.concern_column!=['']:
+	print args.concern_column
+	print('Adding concerns')
+	concern_observed = data_nonzero[data_nonzero.year==2016].groupby('segment_id')[args.concern_column].max()
+	features.append(args.concern_column)
+	data_model = data_model.merge(concern_observed.reset_index(), on='segment_id')
 
 # add in atrs if filepath present
-if args.atr_data!='':
+if args.atr_data!=['']:
 	atrs = pd.read_csv(args.atr_data, dtype={'id':'str'})
 	# for some reason pandas reads the id as float before str conversions
 	atrs['id'] = atrs.id.apply(lambda x: x.split('.')[0])
-	data_segs = data_segs.merge(atrs[['id','speed_coalesced', 'volume_coalesced']], 
+	data_model = data_model.merge(atrs[['id']+args.atr_columns], 
 	                            left_on='segment_id', right_on='id')
+	features += args.atr_columns
 
-# add in tmcs conflicts if filepath present
-if args.tmc_data!='':
+# add in tmcs if filepath present
+if args.tmc_data!=['']:
 	tmcs = pd.read_json(args.tmc_data,
-	                   dtype={'near_id':str})[['near_id','Conflict']]
-	data_segs = data_segs.merge(tmcs, left_on='segment_id', right_on='near_id', how='left')
-	data_segs.Conflict.fillna(0, inplace=True)
+	                   dtype={'near_id':str})[['near_id']+args.tmc_columns]
+	data_model = data_model.merge(tmcs, left_on='segment_id', right_on='near_id', how='left')
+	data_model[args.tmc_columns] = data_model[args.tmc_columns].fillna(0)
+	features += args.tmc_columns
 
+# features for linear model
+lm_features = features
 
-crash_lags = format_crash_data(data_nonzero.set_index(['segment_id','year','week']), 'crash', 
-	week, year)
+if args.process_features!=['False']:
+	for f in f_cat:
+	    t = pd.get_dummies(data_model[f])
+	    t.columns = [f+str(c) for c in t.columns]
+	    data_model = pd.concat([data_model, t], axis=1)
+	    features += t.columns.tolist()
+	    # for linear model, allow for intercept
+	    lm_features += t.columns.tolist()[1:]
+	# aadt - log-transform
+	for f in f_cont:
+		data_model['log_%s' % f] = np.log(data_model[f]+1)
+		features += ['log_%s' % f]
+		lm_features += ['log_%s' % f]
+	# add segment type
+	data_model['intersection'] = data_model.segment_id.map(lambda x: x[:2]!='00').astype(int)
+	features += ['intersection']
+	lm_features += ['intersection']
 
-data_model = crash_lags.merge(data_segs, left_on='segment_id', right_on='segment_id')
-
-# add concerns
-data_model = data_model.merge(concern_observed.reset_index(), on='segment_id')
-
-# trying a different feature set (not implemented)
-#dummy_att = ['SPEEDLIMIT', 'Struct_Cnd', 'Surface_Tp', 'F_F_Class']
-#for d in dummy_att:
-#    t = pd.get_dummies(data_model[d])
-#    t.columns = [d+str(c) for c in t.columns]
-#    data_model = pd.concat([data_model, t], axis=1)
-# aadt - log-transform
-#data_model['log_aadt'] = np.log(data_model.AADT+1)
-# add segment type
-#data_model['intersection'] = data_model.segment_id.map(lambda x: x[:2]!='00').astype(int)
-# features
-#features = data_model.filter(regex='[0-9]').columns.tolist() + ['log_aadt', 'intersection']
-
-features = SEG_CHARS
-
-features += [u'pre_week', u'pre_month', u'pre_quarter', 'avg_week', 'concern_observed']
-features += ['speed_coalesced', 'volume_coalesced']
-features += ['Conflict']
-lm_features = features# list(set(features) - set(['SPEEDLIMIT1', 'Struct_Cnd0', 'F_F_Class0']))
+	# remove duplicated features
+	features = list(set(features) - set(f_cat+f_cont))
+	lm_features = list(set(lm_features) - set(f_cat+f_cont))
 
 #Initialize data
 df = Indata(data_model, 'target')
