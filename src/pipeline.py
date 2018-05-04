e8e9ff38c4479cbe610f0052c1b2233a8b948fe9
Jenny Turner-Trauring <jenineturner@gmail.com>
2018-05-03 20:32:03 -0400

    Allow pipeline to run individual steps

--- src/pipeline.py
+++ src/pipeline.py
@@ -67,6 +67,17 @@ def data_generation(config_file, DATA_FP, start_year=None, end_year=None,
     )
 
 
+def train_model(config_file, DATA_FP):
+    subprocess.check_call([
+        'python',
+        '-m',
+        'models.train_model',
+        '-c',
+        config_file,
+        '-d',
+        DATA_FP
+    ])
+
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
@@ -74,8 +85,16 @@ if __name__ == '__main__':
                         help="config file location")
     parser.add_argument('--forceupdate', action='store_true',
                         help='Whether to force update the maps')
+    # Can also choose which steps of the process to run
+    parser.add_argument('--onlysteps',
+                        help="Give list of steps to run, as comma-separated " +
+                        "string.  Has to be among 'transformation'," +
+                        "'generation', 'model', 'visualization'")
+
 
     args = parser.parse_args()
+    if args.onlysteps:
+        steps = args.onlysteps.split(',')
 
     # Read config file
     with open(args.config_file) as f:
@@ -83,17 +102,18 @@ if __name__ == '__main__':
 
     DATA_FP = os.path.join(BASE_DIR, 'data', config['name'])
 
-    data_transformation(config, DATA_FP, forceupdate=args.forceupdate)
+    if not args.onlysteps or 'transformation' in args.onlysteps:
+        data_transformation(config, DATA_FP, forceupdate=args.forceupdate)
 
-    data_generation(args.config_file, DATA_FP, start_year=config['start_year'],
-                    end_year=config['end_year'], forceupdate=args.forceupdate)
-    # train_model
+    if not args.onlysteps or 'generation' in args.onlysteps:
+        data_generation(args.config_file, DATA_FP,
+                        start_year=config['start_year'],
+                        end_year=config['end_year'],
+                        forceupdate=args.forceupdate)
+
+    if not args.onlysteps or 'model' in args.onlysteps:
+        train_model(args.config_file, DATA_FP)
+
     # risk map
     # visualize
-
-#    subprocess.check_call([
-#        'python',
-#        '-m',
-#        'data_transformation.extract_intersections',
-#        filename])
 
