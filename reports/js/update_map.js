37f043f3c24f83d630f45129ca1d3064feb808bd
Alice Feng <feng.a.g@gmail.com>
2018-05-20 15:18:16 -0400

    Revert viz to display only one map, no more comparison feature

--- reports/js/update_map.js
+++ reports/js/update_map.js
@@ -8,16 +8,6 @@ d3.select('#week_selector').on("input", function() {
 	update_map(+this.value, beforeMap);
 });
 
-// update afterMap when week changes
-d3.select('#compare_week_selector').on('input', function() {
-
-	// update week number displayed next to slider
-	d3.select('#compare_week').text(+this.value);
-	d3.select('#compare_week_selector').property('value', +this.value);
-
-	update_map(+this.value, afterMap);
-})
-
 function update_map(week, map) {
 	//update data displayed on map based on week selected
 	var new_filter = ['all', ['==', 'week', week]];
