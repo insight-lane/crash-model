d857ed12b9a9a0e0ea71b60b8c8884b638a34f72
Jenny Turner-Trauring <jenineturner@gmail.com>
2018-06-04 14:34:08 -0400

    Fix paths for osm_create_maps

--- src/data/tests/test_osm_create_maps.py
+++ src/data/tests/test_osm_create_maps.py
@@ -9,13 +9,13 @@ def test_reproject_and_clean_feats(tmpdir):
 
     tmppath = tmpdir.strpath
     shutil.copy(
-        TEST_FP + '/data/processed/maps/osm.geojson',
+        TEST_FP + '/data/processed/maps/osm_elements.geojson',
         tmppath
     )
 
     # For now, just make sure it runs
     osm_create_maps.clean_ways(
-        tmppath + '/osm.geojson',
+        tmppath + '/osm_elements.geojson',
         tmppath + '/docs'
     )
 
