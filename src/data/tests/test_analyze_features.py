327b0281ebe85e1c1e3f5238f09acb93e188f45f
Jenny Turner-Trauring <jenineturner@gmail.com>
2018-08-01 12:01:41 -0400

    Remove new analysis files - should do this work on a different branch

--- src/data/tests/test_analyze_features.py
+++
@@ -1,9 +0,0 @@
-from .. import analyze_features
-
-
-def test_get_bin():
-    assert analyze_features.get_bin(0, [10, 20, 30]) == 0
-    assert analyze_features.get_bin(6, [10, 20, 30]) == 1
-    assert analyze_features.get_bin(16, [10, 20, 30]) == 2
-    assert analyze_features.get_bin(21, [10, 20, 30]) == 3
-    assert analyze_features.get_bin(33, [10, 20, 30]) == 4
