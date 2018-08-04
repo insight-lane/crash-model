e8442bb0a9d58bad8313577b50dfbbca9354cacd
Jenny <jenineturner@gmail.com>
2018-08-03 20:16:21 -0400

    One more unused import

--- src/visualization/plot_points.py
+++ src/visualization/plot_points.py
@@ -22,7 +22,7 @@ import pandas as pd
 import geopandas as gpd
 from shapely.geometry import Point
 import folium
-from folium import FeatureGroup, Map, CircleMarker
+from folium import FeatureGroup, CircleMarker
 import argparse
 import os
 
