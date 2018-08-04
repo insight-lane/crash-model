5bdab6ca0f8434caf87a18b5ec4adaeded4a8c52
Alice Feng <feng.a.g@gmail.com>
2018-08-04 15:00:08 -0400

    Implement risk score filter

--- reports/js/update_map.js
+++ reports/js/update_map.js
@@ -1,27 +1,16 @@
-// update map when week changes and highlight new bar
-d3.select('#week_selector').on("input", function() {
+// update map when risk score filter changes
+d3.select('#risk_slider').on("input", function() {
 
 	// update week number displayed next to slider
-	d3.select('#week_num').text(+this.value);
-	d3.select('#week_selector').property('value', +this.value);
+	d3.select('#selected_risk').text(+this.value);
+	d3.select('#risk_slider').property('value', +this.value);
 
-	update_map(+this.value, beforeMap);
-
-	highlight_bar(+this.value);
+	update_map(+this.value, map);
 });
 
-function update_map(week, map) {
+function update_map(riskThreshold, map) {
 	//update data displayed on map based on week selected
-	var new_filter = ['all', ['==', 'week', week]];
+	var new_filter = ['all', ['>=', 'prediction', riskThreshold]];
 
-	map.setFilter('crashes', new_filter);
-}
-
-// highlight crash bar of the week selected
-function highlight_bar(week) {
-	d3.select("#weekly_barplot")
-	  .selectAll(".crashbar")
-		.style("fill", "#b2b2b2")
-		.filter(function(d) { return d.week === week ; })
-		.style("fill", "#d500f9");
+	map.setFilter('predictions', new_filter);
 }
