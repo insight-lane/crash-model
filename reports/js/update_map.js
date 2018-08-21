0f0778411259a080330b56f566ff0de789488d72
Alice Feng <feng.a.g@gmail.com>
2018-08-20 22:48:53 -0400

    Populate list of top 10 highest risk segments

--- reports/js/update_map.js
+++ reports/js/update_map.js
@@ -14,3 +14,26 @@ function update_map(riskThreshold, map) {
 
 	map.setFilter('predictions', new_filter);
 }
+
+d3.json("preds_final.json", function(data) {
+	// console.log(data.features[0].properties);
+	var segments = [];
+
+	for (var segment in data.features) {
+		segments.push(data.features[segment].properties);
+	}
+
+	segments.sort(function(a, b) {
+		return d3.descending(a.prediction, b.prediction);
+	})
+
+	// console.log(segments);
+
+	d3.select("#highest_risk_list")
+		.selectAll("li")
+		.data(segments.slice(0, 10))
+		.enter()
+		.append("li")
+		.attr("class", "highRiskSegment")
+		.text(function(d) { return d.segment_display_name; });
+})
