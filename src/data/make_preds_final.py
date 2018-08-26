13a7d8347f52f1c54676e713ed3d97e9c78718ce
Terry Franklin <terryfranklin82@gmail.com>
2018-08-26 11:21:13 +1000

    move make_preds_final away from a single function, use geojson module to create proper structure

--- src/data/make_preds_final.py
+++ src/data/make_preds_final.py
@@ -16,48 +16,29 @@ Output:
 import argparse
 import os
 import pandas as pd
+import geojson
 import json
 
-CURR_FP = os.path.dirname(
-    os.path.abspath(__file__))
-BASE_FP = os.path.dirname(os.path.dirname(CURR_FP))
-
-if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-f", "--folder", type=str,
-                        help="path to city's data folder")
-
-    args = parser.parse_args()
-
-    # confirm files exist
-    segments_file = os.path.join(
-        BASE_FP, args.folder, "processed/maps/inter_and_non_int.geojson")
-    if not os.path.exists(segments_file):
-        print("segment file not found at {}, exiting".format(segments_file))
-        exit(1)
-
-    predictions_file = os.path.join(
-        BASE_FP, args.folder, "processed/seg_with_predicted.json")
-    if not os.path.exists(segments_file):
-        print("predictions file not found at {}, exiting".format(predictions_file))
-        exit(1)
-
-    # load the segments
-    print("loading segments: ", end="")
-    segments_data = pd.read_json(segments_file)
-    # TODO segments data standard should key each segment by its id
-    segments = segments_data["features"]
-    print("{} found".format(len(segments)))
-
+def combine_predictions_and_segments(preds_file, segs_file):
+    """
+    Combine predictions data with certain properties of their related segment.
+    """
+    
     # load the predictions
     print("loading predictions: ", end="")
     predictions = pd.read_json(
-        predictions_file, orient="index", typ="series", dtype=False)
+        preds_file, orient="index", typ="series", dtype=False)
     print("{} found".format(len(predictions)))
+    
+    # load the segments
+    print("loading segments: ", end="")
+    segments_data = pd.read_json(segs_file)
+    # TODO segments data standard should probably key each segment by its id
+    segments = segments_data["features"]
+    print("{} found".format(len(segments)))
 
-    print("matching predictions with segments")
-    preds_final = []
+    print("combining predictions with segments")
+    combined_preds = []
     for pred_id, pred_data in predictions.items():
         for segment in segments:
             # find the matching segment to obtain the display name
@@ -69,24 +50,42 @@ if __name__ == "__main__":
                     "center_y": segment["properties"]["center_y"]
                 }
 
-                preds_final.append({
-                    "type": "Feature",
-                    "properties": pred_data,
-                    "geometry": segment["geometry"]
-                })
+                combined_preds.append(geojson.Feature(
+                    geometry = segment["geometry"],
+                    properties = pred_data,
+                ))
                 break
+    
+    return combined_preds
 
-    # add the assembled predictions into a geoJSON-comaptible structure
-    geo_preds_final = {
-        "type": "FeatureCollection",
-        "features": preds_final
-    }
+def write_preds_as_geojson(preds, outfp):
+    """
+    Output the combined predictions & segments to a geojson file.
+    """
+    
+    preds_collection = geojson.FeatureCollection(preds)
+    with open(outfp, "w") as outfile:
+        geojson.dump(preds_collection, outfile)
+        
+        print("wrote {} assembled predictions to file {}".format(
+        len(preds), outfp))
 
-    geo_preds_final_file = os.path.join(
-        BASE_FP, args.folder, "processed/preds_final.json")
+if __name__ == "__main__":
 
-    with open(geo_preds_final_file, "w") as f:
-        json.dump(geo_preds_final, f)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--folder", type=str,
+                        help="path to destination's data folder")
 
-    print("wrote {} assembled predictions to file {}".format(
-        len(preds_final), geo_preds_final_file))
+    args = parser.parse_args()
+
+    # confirm required files exist
+    predictions_file = os.path.join(args.folder, "processed/seg_with_predicted.json")
+    if not os.path.exists(predictions_file):
+        sys.exit("predictions file not found at {}, exiting".format(predictions_file))
+        
+    segments_file = os.path.join(args.folder, "processed/maps/inter_and_non_int.geojson")
+    if not os.path.exists(segments_file):
+        sys.exit("segment file not found at {}, exiting".format(segments_file))
+    
+    preds_final = combine_predictions_and_segments(predictions_file, segments_file)
+    write_preds_as_geojson(preds_final, os.path.join(args.folder, "processed/preds_final.geojson"))
