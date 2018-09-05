c781d82ae0ffbc2ba32c740d791ea7c184409f29
Alice Feng <feng.a.g@gmail.com>
2018-09-04 21:30:00 -0400

    Make prediction bar wider

--- reports/js/update_map.js
+++ reports/js/update_map.js
@@ -90,7 +90,7 @@ function makeBarChart(prediction, median) {
 
 	var margin = {top: 0, right: 0, bottom: 48, left: 0},
 		width = 250,
-		height = 8;
+		height = 10;
 
 	xScale.rangeRound([0, width]);
 
