25ea335236fea30e2ad093bbc6832f8229607775
Jenny <jenineturner@gmail.com>
2018-10-19 08:13:04 -0400

    Write to file

--- src/data/add_waze_data.py
+++ src/data/add_waze_data.py
@@ -22,14 +22,35 @@ def get_linestring(value):
 
 
 def get_features(waze_info, properties, num_snapshots):
-
+    """
+    Given a dict with keys of segment id, and val a list of waze jams
+    (for now, just jams), the properties of a road segment, and the
+    total number of snapshots we're looking at, update the road segment's
+    properties to include features
+    Args:
+        waze_info - dict
+        properties - dict
+        num_snapshots
+    Returns
+        properties
+    """
+    # Waze feature list
+    # jam_percent - percentage of snapshots that have a jam on this segment
     if properties['segment_id'] in waze_info:
         # only count one jam per snapshot on a road
-        num_jams = len(waze_info[properties['segment_id']])
-
+        num_jams = len(set([x['properties']['snapshotId']
+                            for x in waze_info[properties['segment_id']]]))
     else:
         num_jams = 0
     properties.update(jam_percent=num_jams/num_snapshots)
+
+    # Other potential features
+    # Something with speeds in the jams
+    # Alerts, or certain kinds of alerts
+    # Look at alerts that are crashes, maybe ignore those jams?
+    # Might be interesting to look at crashes on segments as well
+    #   but not as a feature for the model
+
     return properties
 
 
@@ -41,12 +62,13 @@ def map_segments(datadir, filename):
     # Get the total number of snapshots in the waze data
     num_snapshots = max([x['properties']['snapshotId'] for x in items])
 
-    road_segments, _ = util.get_roads_and_inters(os.path.join(
+    osm_file = os.path.join(
         datadir,
         'processed',
         'maps',
         'osm_elements.geojson'
-    ))
+    )
+    road_segments, _ = util.get_roads_and_inters(osm_file)
     roads, roads_index = util.index_segments(
         road_segments, geojson=True, segment=True)
     road_buffers = []
@@ -80,10 +102,18 @@ def map_segments(datadir, filename):
             road.properties,
             num_snapshots
         )
-        updated_roads.append(
-            {'geometry': road.geometry, 'properties': properties})
+        updated_roads.append({
+                'geometry': {
+                    'coordinates': [x for x in road.geometry.coords],
+                    'type': 'LineString'
+                },
+                'properties': properties
+        })
 
-    # Look at alerts that are crashes, maybe ignore those jams?
+    results = util.prepare_geojson(updated_roads)
+
+    with open(osm_file, 'w') as outfile:
+        geojson.dump(results, outfile)
 
 
 def make_map(filename, datadir):
