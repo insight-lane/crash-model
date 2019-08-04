1e027c9643d4af70b48df5b83428e8897a02f4fb
Terry Franklin <terryfranklin82@gmail.com>
2019-08-04 20:33:02 +1000

    gcp showcase hosting setup

---
+++ src/showcase/static/gcp_config.js
@@ -0,0 +1,51 @@
+var config = [
+  {
+      id: "boston",
+      name: "Boston, Massachusetts, USA",
+      latitude: 42.3600825,
+      longitude: -71.0588801,
+      file: "https://storage.googleapis.com/insightlane-showcase/data/boston/preds_viz.geojson",
+      crashes: "https://storage.googleapis.com/insightlane-showcase/data/boston/crashes_rollup.geojson"
+  },
+  {
+      id: "cambridge",
+      name: "Cambridge, Massachusetts, USA",
+      latitude: 42.3736158,
+      longitude: -71.10973349999999,
+      file: "https://storage.googleapis.com/insightlane-showcase/data/cambridge/preds_viz.geojson",
+      crashes: "https://storage.googleapis.com/insightlane-showcase/data/cambridge/crashes_rollup.geojson"
+  },
+  {
+      id: "dc",
+      name: "Washington D.C, USA",
+      latitude: 38.9071923,
+      longitude: -77.0368707,
+      file: "https://storage.googleapis.com/insightlane-showcase/data/dc/preds_viz.geojson",
+      crashes: "https://storage.googleapis.com/insightlane-showcase/data/dc/crashes_rollup.geojson"
+  },
+  {
+    id: "la",
+    name: "Los Angeles, USA",
+    latitude: 34.052235,
+    longitude: -118.243683,
+    file: "https://storage.googleapis.com/insightlane-showcase/data/losangeles/preds_viz.geojson",
+    crashes: "https://storage.googleapis.com/insightlane-showcase/data/losangeles/crashes_rollup.geojson"
+},
+  {
+      id: "brisbane",
+      name: "Brisbane, Australia",
+      latitude: -27.4697707,
+      longitude: 153.0251235,
+      file: "https://storage.googleapis.com/insightlane-showcase/data/brisbane/preds_viz.geojson",
+      crashes: "https://storage.googleapis.com/insightlane-showcase/data/brisbane/crashes_rollup.geojson"
+  },
+  {
+      id: "melbourne",
+      name: "Melbourne, Australia",
+      latitude: -37.814,
+      longitude: 144.96332,
+      file: "https://storage.googleapis.com/insightlane-showcase/data/melbourne/preds_viz.geojson",
+      crashes: "https://storage.googleapis.com/insightlane-showcase/data/melbourne/crashes_rollup.geojson"
+  }
+
+]
